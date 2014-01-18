09ebb51247bbbf2da0d31dc1c62a4bcebdbfcf59
hissy <hishikawa@concrete5.co.jp>
2014-01-18 20:39:33 +0900

    Check permission and block type handle

--- web/tools/page_list.php
+++ web/tools/page_list.php
@@ -1,26 +1,45 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied.");
 
+// Validate request
 if (!Loader::helper('validation/numbers')->integer($_REQUEST['bID'])) {
 	die(t('Access Denied'));
 }
 
+// Get Block object
 $bID = $_REQUEST['bID'];
 $b = Block::getByID($bID);
 
+// Check Block exists
 if (is_object($b) && !$b->isError()) {
 	
+	// Check block type handle
+	if ($b->getBlockTypeHandle() != 'page_list') {
+		die(t('Access Denied'));
+	}
+	
+	// Check whether or not to user can read the block
+	$bp = new Permissions($b);
+	if (!$bp->canRead()) {
+		die(t('Access Denied'));
+	}
+	
+	// Get block type controller
 	$btc = $b->getController();
+	
+	// Get PageList object
 	$pl = $btc->getPageList();
 	
+	// Get pages
 	if ($pl->getItemsPerPage() > 0) {
 		$pages = $pl->getPage();
 	} else {
 		$pages = $pl->get();
 	}
 	
+	// Load helpers
 	$nh = Loader::helper('navigation');
 	
-	//Pagination...
+	// Pagination...
 	$showPagination = false;
 	$paginator = null;
 	if ($btc->paginate && $btc->num > 0 && is_object($pl)) {
